37a1e24b7f5ab200f2296b2cd457e5f3b158a8cd
gyungchan Jo <namejgc@naver.com>
2023-11-12 01:00:45 +0900

    feat(InputValidator): 예상 방문 날짜에 대한 입력을 검증하는 로직 구현

--- src/main/java/christmas/view/validator/InputValidator.java
+++ src/main/java/christmas/view/validator/InputValidator.java
@@ -2,12 +2,30 @@ package christmas.view.validator;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
     private static final String INVALID_INPUT_EXCEPTION_MESSAGE = "입력은 공백일 수 없습니다.";
+    private static final String NOT_DIGIT_EXCEPTION_MESSAGE = "예상 방문 날짜에 대한 입력은 숫자만 가능합니다.";
+
+    private static final Pattern ONLY_DIGIT = Pattern.compile("-?[0-9]+");
 
     protected static void validateInput(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
         }
+    }
+
+    public static void validateDate(String input) {
+        validateInput(input);
+
+        if (isNotDigit(input)) {
+            throw new IllegalArgumentException(NOT_DIGIT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static boolean isNotDigit(String input) {
+        return !ONLY_DIGIT.matcher(input)
+                .matches();
     }
 }
