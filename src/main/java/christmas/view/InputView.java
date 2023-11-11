24d29d3719f763310a00f80c25f670b3a80315ac
gyungchan Jo <namejgc@naver.com>
2023-11-12 01:19:44 +0900

    feat(InputView): 주문할 메뉴와 개수를 입력받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,12 +5,25 @@ import christmas.view.validator.InputValidator;
 
 public class InputView {
     private static final String INPUT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String INPUT_MENU_AND_COUNT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public int readDate() {
         System.out.println(INPUT_DATE_MESSAGE);
-        String input = Console.readLine();
+        String input = readLine();
 
         InputValidator.validateDate(input);
         return Integer.parseInt(input);
+    }
+
+    public String readMenuAndCount() {
+        System.out.println(INPUT_MENU_AND_COUNT_MESSAGE);
+        String input = readLine();
+
+        InputValidator.validateMenuAndCount(input);
+        return input;
+    }
+
+    private String readLine() {
+        return Console.readLine();
     }
 }
