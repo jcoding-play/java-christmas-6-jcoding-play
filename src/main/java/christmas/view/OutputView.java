5baf5c7781558c427b444a2a999705dd58c64e52
gyungchan Jo <namejgc@naver.com>
2023-11-13 13:12:15 +0900

    feat(OutputView): 이벤트 혜택을 미리 보여준다는 메시지를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,8 +2,13 @@ package christmas.view;
 
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PREVIEW_BENEFITS_MESSAGE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public void printPreviewBenefitsMessage(int month, int date) {
+        System.out.printf(PREVIEW_BENEFITS_MESSAGE_FORMAT, month, date);
     }
 }
