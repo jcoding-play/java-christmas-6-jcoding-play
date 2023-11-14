613467b15249151347f822307f9d73e094596927
gyungchan Jo <namejgc@naver.com>
2023-11-15 01:45:26 +0900

    test(EventBenefitsTest): 증정 메뉴를 찾는 기능 테스트

--- src/test/java/christmas/domain/benefit/EventBenefitsTest.java
+++ src/test/java/christmas/domain/benefit/EventBenefitsTest.java
@@ -2,6 +2,7 @@ package christmas.domain.benefit;
 
 import christmas.domain.event.*;
 import christmas.domain.menu.Drink;
+import christmas.domain.menu.Main;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -36,5 +37,28 @@ class EventBenefitsTest {
         int estimatedPaymentAmount = eventBenefits.calculateEstimatedPaymentAmount(totalOrderPrice);
 
         assertThat(estimatedPaymentAmount).isEqualTo(135754);
+    }
+
+    @Test
+    @DisplayName("증정 이벤트에 해당하는 경우, 증정 메뉴를 찾을 수 있다.")
+    void findGiftMenu_V1() {
+        Map<Event, Integer> result = Map.of(new GiftEvent(new GiftMenu(Main.T_BONE_STREAK, 1)), 25000);
+        EventBenefits eventBenefits = new EventBenefits(result);
+
+        GiftMenu giftMenu = eventBenefits.findGiftMenu();
+
+        assertThat(giftMenu).isNotNull();
+        assertThat(giftMenu.getName()).isEqualTo(Main.T_BONE_STREAK.getName());
+    }
+
+    @Test
+    @DisplayName("증정 이벤트에 해당하지 않는 경우, 증정 메뉴를 찾을 수 없다.")
+    void findGiftMenu_V2() {
+        Map<Event, Integer> result = Map.of(new SpecialDiscount(), 1000);
+        EventBenefits eventBenefits = new EventBenefits(result);
+
+        GiftMenu giftMenu = eventBenefits.findGiftMenu();
+
+        assertThat(giftMenu).isNull();
     }
 }
