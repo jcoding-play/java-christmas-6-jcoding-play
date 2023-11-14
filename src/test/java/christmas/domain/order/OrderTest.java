c1eb7ddb499d502f0d30cd5176cf7b0b61ea1c29
gyungchan Jo <namejgc@naver.com>
2023-11-15 02:24:15 +0900

    test(OrdersTest): 디저트와 메인 메뉴 개수를 세는 기능 테스트

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -1,33 +1,26 @@
 package christmas.domain.order;
 
-import christmas.domain.menu.Drink;
 import christmas.domain.menu.Main;
-import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderTest {
 
     @Test
-    @DisplayName("전체 주문 내역을 알 수 있다.")
+    @DisplayName("주문할 메뉴와 개수를 알 수 있다.")
     void createOrder() {
-        Orders orders = new Orders(List.of(new Order(Main.T_BONE_STREAK, 1), new Order(Drink.ZERO_COLA, 1)));
-
-        assertThat(orders).extracting("orders", InstanceOfAssertFactories.list(Order.class))
-                .containsExactly(new Order(Main.T_BONE_STREAK, 1), new Order(Drink.ZERO_COLA, 1));
+        Order order = new Order(Main.T_BONE_STREAK, 1);
+        assertThat(order).isEqualTo(new Order(Main.T_BONE_STREAK, 1));
     }
 
     @Test
-    @DisplayName("할인 전 총주문 금액을 계산할 수 있다.")
-    void calculateTotalOrderPrice() {
-        Orders orders = new Orders(List.of(new Order(Main.T_BONE_STREAK, 1), new Order(Drink.ZERO_COLA, 3)));
-
-        int result = orders.calculateTotalOrderAmount();
-
-        assertThat(result).isEqualTo(64000);
+    @DisplayName("주문한 메뉴의 개수가 1보다 작을 경우 예외가 발생한다.")
+    void lessThanMinimumOrderCount() {
+        assertThatThrownBy(() -> new Order(Main.T_BONE_STREAK, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
